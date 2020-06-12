283310fea23d0e5d70ec2a571cf5909e1a1b068a
unknown <nikoladimitrijevic1991@gmail.com>
2020-06-12 10:07:35 +0200

    homepage

--- home-page/js/homePage.js
+++ home-page/js/homePage.js
@@ -12,3 +12,13 @@ searchInput[0].addEventListener('focusout',()=>{
   inputBox[0].style.width = '50%';
   mainHeading[0].style.opacity = '1';
 })
+
+
+
+// middlePaper anim
+var middlePaper = document.getElementsByClassName('middlePaper');
+
+window.addEventListener('load',()=>{
+  middlePaper[0].style.top = '-9.5vw';
+  middlePaper[0].style.boxShadow = '0 0 9vw 1vw rgba(0,0,0,0.2)'
+})
